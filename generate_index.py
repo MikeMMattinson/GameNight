b02e79e20d076ebb4b845f793479b2e9b36076f0
Mike Mattinson <mike.mattinson@gmail.com>
2025-06-12 15:12:32 -0400

    no cache

--- generate_index.py
+++ generate_index.py
@@ -40,6 +40,9 @@ html_content = """
 <head>
     <meta charset="UTF-8">
     <title>Game Night</title>
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
     <link rel="stylesheet" href="../styles.css">
 </head>
 <body>
